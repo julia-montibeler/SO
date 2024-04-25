46f2489041492e5a764505bc97c275d23a2f2248
julia-montibeler <juliamaralbeler@gmail.com>
2024-04-24 21:11:10 -0300

    shift e caps

--- kbd.c
+++ kbd.c
@@ -2,22 +2,78 @@
 #include "printf.h"
 #include "x86.h"
 
+static int shift = 0;
+static int caps = 0;
+
 static char sc[] = {
     ' ', ' ', '1', '2', '3', '4', '5', '6', '7', '8', 
     '9', '0', '-', '=', '`', ' ', 'q', 'w', 'e', 'r', 
     't', 'y', 'u', 'i', 'o', 'p', '[', ']', ' ', ' ',
     'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', 
     '"', ' ', ' ', ' ', 'z', 'x', 'c', 'v', 'b', 'n', 
-    'm', ',', '.', '/', ' ', ' ', ' ', 'a'};
+    'm', ',', '.', '/', ' ', ' ', ' ', ' '};
+
+static unsigned char scShift[] = {
+    ' ', ' ', '!', '@', '#', '$', '%', '¨', '&', '*', 
+    '(', ')', '-', '=', '`', ' ', 'Q', 'W', 'E', 'R', 
+    'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', ' ', ' ',
+    'A', 'S', 'D', 'F', 'G', 'H', 'I', 'J', 'L', ':', 
+    '"', ' ', ' ', ' ', 'Z', 'X', 'C', 'V', 'B', 'N', 
+    'M', '<', '>', '?', ' ', ' ', ' ', ' '};
 
 /* scancode to ascii */
 void
-sctoa(char scod)
+sctoa(unsigned char scod)
 {
 
+
+    //Verificar caps
+    if (scod == 0x3A)
+    {
+        if (caps)
+        {
+            caps = 0;
+            return;
+        }
+        caps = 1;
+        return;
+    }
+
+    //Verificar shift
+    if (scod == 0x2A || scod == 0x36) {
+        shift = 1;
+        return;
+    }
+
+    if (scod == 0xAA || scod == 0xB6) {
+        shift = 0;
+        return;
+    }
+    
+    
+    //Printar de acordo
     if (scod >= 0 && scod <= 0x39) {
+        if (shift) {
+            if (caps) {
+                if (sc[scod] >= 97 && sc[scod] <= 122) {
+                    printc(toLowerCase(scShift[scod]));
+                    return;
+                }
+            }
+            printc(scShift[scod]);
+            return;
+        }
+        if (caps)
+        {
+            if (sc[scod] >= 97 && sc[scod] <= 122)
+            {
+                printc(toUpperCase(sc[scod]));
+                return;
+            }
+
+        }
+        
         printc(sc[scod]);
-    } else if ((scod - 0x80) <= 0x39) {
     }
 }
 
@@ -27,4 +83,12 @@ kbd_isr()
 {
 	eoi(PIC1_CTLR);
 	sctoa(inb(0x60));
+}
+
+int toUpperCase(int c) {
+    return c-32;
+}
+
+int toLowerCase(int c) {
+    return c+32;
 }
